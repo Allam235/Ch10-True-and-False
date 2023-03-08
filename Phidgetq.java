f9c79a0b452808a80dc824eab32b67123ac8dfea
Allam235 <106848509+Allam235@users.noreply.github.com>
2023-03-08 09:29:01 -0500

    Update Phidgetq.java

--- Phidgetq.java
+++ Phidgetq.java
@@ -1,4 +1,12 @@
 //Rithvik Allamaneni
+/*
+Questions:  
+A Call Stack is the different variables that are returned by each recursive call. (False)   
+Merge Sort is fastest when the numbers are already sorted. (False)   
+A base case is when the method no longer calls itself. (True)   
+Recursion is always better than or as good as a loop. (False)  
+*/
+
 package gettingstarted;
 
 //Add Phidgets Library 
